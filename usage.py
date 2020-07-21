d1da07eb6f5ae6320e497cd7c40cb897d182ee31
abhijeet <abhijeet.pal@capitalnumbers.com>
2020-07-21 17:03:50 +0530

    more tests added

--- usage.py
+++ usage.py
@@ -6,6 +6,21 @@ pricing_rules = [{'B': 10, 'R': 5, 'ST': 2.5}, {'bonus_chart':
                                                  {'item': 'R', 'quantity_more_than': 8, 'bonus_amount': 10, 'type': 'percentage'}]}]
 
 
+# Test case 1
+calculator = Calculator(pricing_rules)
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('R')
+calculator.add('R')
+calculator.add('ST')
+total = calculator.total()
+print(total)
+
+# Test case 2
 calculator = Calculator(pricing_rules)
 calculator.add('B')
 calculator.add('R')
@@ -18,7 +33,60 @@ calculator.add('R')
 calculator.add('R')
 calculator.add('R')
 calculator.add('R')
+total = calculator.total()
+print(total)
 
 
+# Test case 3
+calculator = Calculator(pricing_rules)
+calculator.add('B')
+calculator.add('R')
+calculator.add('ST')
+calculator.add('ST')
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+total = calculator.total()
+print(total)
+
+# Test case 4
+calculator = Calculator(pricing_rules)
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('R')
+calculator.add('ST')
+calculator.add('ST')
+calculator.add('R')
+calculator.add('R')
+total = calculator.total()
+print(total)
+
+
+pricing_rules = [{'B': 10, 'R': 5, 'ST': 2.5}, {'bonus_chart':
+                                                [{'item': 'R', 'quantity_more_than': 2, 'bonus_amount': 100, 'type': 'percentage'}]}]
+
+
+# Test case 5
+calculator = Calculator(pricing_rules)
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+total = calculator.total()
+print(total)
+
+
+# Test case 5
+calculator = Calculator(pricing_rules)
+calculator.add('B')
+calculator.add('B')
+calculator.add('B')
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+calculator.add('R')
+calculator.add('ST')
 total = calculator.total()
 print(total)
